40572b4fadd3638498a55e8f8a7e33f917361f9c
Blackkspydo <Blackkspydo@github.com>
2020-06-26 10:56:14 +0545

    First commit
    Signed-off-by: Blackkspydo <Blackkspydo@github.com>

--- project_55.py
+++ project_55.py
@@ -1,22 +1,19 @@
-# How to bubble sort elements in array?
+# How to selection sort elements in array?
 from random import randint
-def minimum(array):
-    minimum = 0
-    for i in array:
-        if minimum > i:
-            minimum = i
-    return minimum
 
-x = 10
+x = 20
 array = []
-for i in range(x):
-    array.append(randint(1,20))
+
+for i in range(20):
+    array.append(randint(1, 20))
 print(f'Unsorted:{array}')
 
-for i in range(x):
-    for j in range(x-i):
-        m = minimum(array[i:])
-        k = array[j]
-        array[j] = array[i]
-        array[i] = k
-print(array)
+j = x-1
+while j != 0:
+    k = 0
+    for i in range(1,j+1):
+        if array[i] > array[k]:
+            k = i
+    array[k], array[j] = array[j], array[k]
+    j -= 1
+print(f'Sorted array:{array}')
